2f584b0ee4e6c3228697db1b8d437d8d9956a221
Rishi Raj <rishiraj824@gmail.com>
2016-08-06 21:04:27 +0530

    init

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-angular.module("app", ["chart.js"]).controller("LineCtrl", function ($scope) {
+var app = angular.module("app", ["chart.js"]);
+app.controller("LineCtrl", function ($scope) {
 
   $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
   $scope.series = ['Series A', 'Series B'];
@@ -28,4 +29,13 @@ angular.module("app", ["chart.js"]).controller("LineCtrl", function ($scope) {
       ]
     }
   };
+});
+app.controller("BarCtrl", function ($scope) {
+  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.series = ['Series A', 'Series B'];
+
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
 });
